7e19815b7601f2230cfb74240a918282d3625731
gcousin <gab-cousin@hotmail.fr>
2022-01-06 16:05:10 +0100

    first commit

---
+++ app/src/main/java/com/m2/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.m2.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startCourses(View view) {
+        // New activity
+    }
+
+    public void historicCourses(View view) {
+        // new Acitivity
+    }
+}
