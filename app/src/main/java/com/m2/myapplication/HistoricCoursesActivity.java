1acffd434c30df9d2c51f3d480a36690ed00fc4c
gcousin <gab-cousin@hotmail.fr>
2022-01-09 18:55:24 +0100

    historic and info

--- app/src/main/java/com/m2/myapplication/HistoricCoursesActivity.java
+++ app/src/main/java/com/m2/myapplication/HistoricCoursesActivity.java
@@ -1,13 +1,71 @@
 package com.m2.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import com.m2.myapplication.database.Course;
+import com.m2.myapplication.database.CourseTrackingDB;
+import com.m2.myapplication.database.Position;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HistoricCoursesActivity extends AppCompatActivity {
+
+    public List<Course> list;
+    ArrayAdapter<Course> adapter;
+    ListView courseListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historic_courses);
+
+        list = new ArrayList<>();
+        adapter = new ArrayAdapter<Course>(this, android.R.layout.simple_list_item_1, list);
+        courseListView = (ListView) findViewById(R.id.list);
+        courseListView.setAdapter(adapter);
+        registerForContextMenu(courseListView);
+
+        courseListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView <? > arg0, View view, int position, long id) {
+                Intent intentInfo = new Intent(HistoricCoursesActivity.this, InfoCourseActivity.class);
+                Bundle data = new Bundle();
+                data.putString("courseId", HistoricCoursesActivity.this.list.get(position).getIdCourse());
+                intentInfo.putExtras(data);
+                startActivity(intentInfo);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getListCourse();
+    }
+
+    public void getListCourse() {
+        this.list.clear();
+        new Thread(() -> {
+            CourseTrackingDB db = Room
+                    .databaseBuilder(
+                            getApplicationContext(),
+                            CourseTrackingDB.class,
+                            "courseTracking")
+                    .build();
+
+            List<Course> exams = db.courseDao().getAll();
+            for (Course exam : exams) {
+                this.list.add(exam);
+            }
+        }).start();
     }
 }
---
+++ app/src/main/java/com/m2/myapplication/InfoCourseActivity.java
@@ -0,0 +1,77 @@
+package com.m2.myapplication;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import com.m2.myapplication.database.Course;
+import com.m2.myapplication.database.CourseDao;
+import com.m2.myapplication.database.CourseTrackingDB;
+import com.m2.myapplication.database.Position;
+
+import java.text.DateFormat;
+import java.util.List;
+
+public class InfoCourseActivity extends AppCompatActivity {
+
+    private Course course;
+
+    private TextView textUser;
+    private TextView textDateStart;
+    private TextView textDateEnd;
+    private TextView textNbStep;
+    private TextView textNbMetre;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_course);
+
+        Bundle b = getIntent().getExtras();
+        String courseId   = b.getString("courseId");
+
+        this.textUser = findViewById(R.id.text_info_user);
+        this.textDateStart = findViewById(R.id.text_info_date_start);
+        this.textDateEnd = findViewById(R.id.text_info_date_end);
+        this.textNbStep = findViewById(R.id.text_info_nb_steps);
+        this.textNbMetre = findViewById(R.id.text_info_nb_meter);
+
+        new Thread(() -> {
+            CourseTrackingDB db = Room
+                    .databaseBuilder(
+                            getApplicationContext(),
+                            CourseTrackingDB.class,
+                            "courseTracking")
+                    .fallbackToDestructiveMigration()
+                    .build();
+
+            course = db.courseDao().getById(courseId);
+            List<Position> positions = db.positionDao().getAllByIdCourse(courseId);
+
+            Log.d("TEST", course.toString());
+            for (Position position : positions) {
+                Log.d("TEST", position.toString());
+            }
+
+            DateFormat mediumDateFormat = DateFormat.getDateTimeInstance(
+                    DateFormat.MEDIUM,
+                    DateFormat.MEDIUM);
+
+            this.textDateStart.setText("Date de début :  " + mediumDateFormat.format(this.course.dateStart));
+            this.textDateEnd.setText("Date de début :  " + mediumDateFormat.format(this.course.dateEnd));
+            this.textUser.setText("User : " + this.course.getIdUser());
+            this.textNbStep.setText("Nombre de pas : " + this.course.getNbSteps() + " pas");
+            this.textNbMetre.setText("Nombre de metre : " + this.course.getNbMetre() + " m");
+
+
+        }).start();
+    }
+
+    public void SeeMap(View view) {
+
+    }
+}
